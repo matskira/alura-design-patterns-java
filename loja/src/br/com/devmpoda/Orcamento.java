bc3a61e8c60cadbb9049aee04285645eab61715c
Matheus Poda <matheus.poda@gmail.com>
2023-07-30 16:11:36 -0300

    Adicionado funcionalidade de aplicar desconto extra

--- loja/src/br/com/devmpoda/Orcamento.java
+++ loja/src/br/com/devmpoda/Orcamento.java
@@ -6,10 +6,26 @@ public class Orcamento {
 
     private BigDecimal valor;
     private int quantidadeItens;
+    private String situacao;
 
     public Orcamento(BigDecimal valor, int quantidadeItens) {
         this.valor = valor;
         this.quantidadeItens = quantidadeItens;
+    }
+
+    public void aplicarDescontoExtra() {
+        BigDecimal valorDoDescontoExtra = BigDecimal.ZERO;
+        if ("EM ANALISE".equals(situacao)) {
+            valorDoDescontoExtra = new BigDecimal("0.05");
+        } else if ("APROVADO".equals(situacao)) {
+            valorDoDescontoExtra = new BigDecimal("0.02");
+        }
+
+        this.valor = this.valor.subtract(valorDoDescontoExtra);
+    }
+
+    public void aprovar() {
+        this.situacao = "APROVADO";
     }
 
     public BigDecimal getValor() {
