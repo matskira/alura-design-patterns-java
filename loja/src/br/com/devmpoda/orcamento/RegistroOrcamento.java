1040a1f40c4fb289cc5c606048228e4c42c2320f
Matheus Poda <matheus.poda@gmail.com>
2023-08-09 20:59:05 -0300

    Adiciona funcionalidade para registrar orcamentos em uma API externa

---
+++ loja/src/br/com/devmpoda/orcamento/RegistroOrcamento.java
@@ -0,0 +1,9 @@
+package br.com.devmpoda.orcamento;
+
+public class RegistroOrcamento {
+
+    public void registrar(Orcamento orcamento) {
+        // Aqui poderia ser um DAO, um serviço, etc.
+        System.out.println("Orçamento registrado!");
+    }
+}
